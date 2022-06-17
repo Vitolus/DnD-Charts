a1ea85cf185f236a65d36c657bf33e006fddfcd6
Vitolus <vito.davide00@gmail.com>
2022-06-17 17:27:29 +0200

    Update record.cpp

--- model/record.cpp
+++ model/record.cpp
@@ -5,9 +5,12 @@ Record::Record(QString r, QString c, QString a, unsigned int l)
 
 const QString &Record::getRazza() const{return razza;}
 void Record::setRazza(const QString &newRazza){razza = newRazza;}
+
 const QString &Record::getClasse() const{return classe;}
 void Record::setClasse(const QString &newClasse){classe = newClasse;}
+
 const QString &Record::getAllineamento() const{return allineamento;}
 void Record::setAllineamento(const QString &newAllineamento){allineamento = newAllineamento;}
+
 unsigned int Record::getLivello() const{return livello;}
 void Record::setLivello(unsigned int newLivello){livello = newLivello;}
